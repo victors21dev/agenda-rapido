3bf4abd7bb18de74ef9bebed345bea5d67b888d1
Victor Santos <santos.victors2000@gmail.com>
2026-02-26 18:35:39 -0300

    chore: Config initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button className="bg-blue-500 text-white">
+          Teste shadcn com tailwindcss
+        </Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
