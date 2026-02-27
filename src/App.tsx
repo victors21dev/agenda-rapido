ad643d36dc99a67c871c40a616cfacdef54a5287
Victor Santos <santos.victors2000@gmail.com>
2026-02-27 00:10:15 -0300

    feat: Added theme switching with toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 
+import { ThemeProvider } from "./components/layout/ThemeProvider";
 import BusinessHoursPage from "./pages/BusinessHours";
 import ClientsPage from "./pages/Clients";
 import EventsPage from "./pages/Events";
@@ -7,17 +8,19 @@ import Scheduling from "./pages/Scheduling";
 
 function App() {
   return (
-    <BrowserRouter>
-      <main className="flex bg-background flex-col w-full h-full text-foreground">
-        <Routes>
-          <Route path="/agendamentos" element={<Scheduling />} />
-          <Route path="/eventos" element={<EventsPage />} />
-          <Route path="/clientes" element={<ClientsPage />} />
-          <Route path="/funcionamento" element={<BusinessHoursPage />} />
-          <Route path="*" element={<h1>404 - Página não encontrada</h1>} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <main className="flex bg-background flex-col w-full h-full text-foreground poppins-regular">
+          <Routes>
+            <Route path="/agendamentos" element={<Scheduling />} />
+            <Route path="/eventos" element={<EventsPage />} />
+            <Route path="/clientes" element={<ClientsPage />} />
+            <Route path="/funcionamento" element={<BusinessHoursPage />} />
+            <Route path="*" element={<h1>404 - Página não encontrada</h1>} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,5 @@
+import { ModeToggle } from "./ModeToggle";
+
 type HeaderProps = {
   title: string;
   description: string;
@@ -5,9 +7,15 @@ type HeaderProps = {
 
 const Header = ({ title, description }: HeaderProps) => {
   return (
-    <div>
-      <h1 className="text-lg font-bold ">{title}</h1>
-      <p className="text-sm text-muted-foreground">{description}</p>
+    <div className="w-full flex justify-between">
+      <div>
+        <h1 className="text-lg font-bold ">{title}</h1>
+        <p className="text-sm text-muted-foreground">{description}</p>
+      </div>
+      <div className="flex items-center gap-2">
+        Tema
+        <ModeToggle />
+      </div>
     </div>
   );
 };
